82f97fb675f1450993dfa51fa81a250f5db39524
Nikhil Garia <53515153+nikhil-garia@users.noreply.github.com>
2025-12-11 07:13:33 -0800

    Delete frontend/src/environments/environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment={
-    production:true,
-    apiUrl:`https://praveentechsol.online:8080`,
-    clientId:'your-google-client-id-here' // Temporary placeholder - replace with actual Google Client ID
-}
